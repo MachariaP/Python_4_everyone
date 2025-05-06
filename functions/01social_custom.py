ee3d26ccfefb37d8a035cdb62d1a02568cc9c4ef
Phinehas <walburphinehas78@gmail.com>
2025-05-06 10:57:05 +0300

    Implemented the social custom Python Hungarian way

---
+++ functions/01social_custom.py
@@ -0,0 +1,9 @@
+# Implementing the social custom Python Hungarian way
+
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+
+introduction("Skywalker", "Luke") 
+introduction("Quick", "Jesse")
+introduction("Kent", "Clark")
+
