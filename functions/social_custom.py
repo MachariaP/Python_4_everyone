a22c708974d151520ea8c9ab28673ab6e5333f9f
Phinehas <walburphinehas78@gmail.com>
2025-05-06 10:53:07 +0300

    Implemented the social custom in Python

---
+++ functions/social_custom.py
@@ -0,0 +1,10 @@
+# Implementing the social custom in python
+
+def introduction(first_name, last_name, age):
+    print(f"Hello, my name is {first_name} {last_name} and I am {age} years old.")
+
+# Example usage
+introduction("John", "Doe", 30)  # Hello, my name is John Doe and I am 30 years old.
+introduction("Luke", "Skywalker", 19)  # Hello, my name is Luke Skywalker and I am 19 years old.
+introduction("Leia", "Organa", 22)  # Hello, my name is Leia Organa and I am 22 years old.
+introduction("Han", "Solo", 35)  # Hello, my name is Han Solo and I am 35 years old.
