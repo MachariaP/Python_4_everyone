955599cca4750134490c6b1cf107c97a6d1e99f5
Phinehas <walburphinehas78@gmail.com>
2025-05-06 10:46:00 +0300

    Positional parameter passing 'assigns the i-th argument to the i-th parameter'

---
+++ functions/parameter.py
@@ -0,0 +1,7 @@
+# Positional parameter passing (assigns the ith argument to the i-th parameter)
+
+def my_function(a, b, c):
+    print(f"a: {a}, b: {b}, c: {c}")
+
+# Positional arguments
+my_function(1, 2, 3)  # a: 1, b: 2, c: 3
