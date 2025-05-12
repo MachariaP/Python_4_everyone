a3f664a5ff8b2e264ddcff8bdaf30d7eaa44ca4a
Phinehas <walburphinehas78@gmail.com>
2025-05-12 11:39:33 +0300

    Add randrange.py to demonstrate randrange and randint usage
    - Implemented script to generate and print random integers using random.randrange and random.randint.
    - Used randrange with different parameters to show range behavior (always 0 for [0, 1)).
    - Imported randrange and randint from random module's namespace.
    - Included docstring explaining purpose of random integer generation.

---
+++ modules/selected_python_module/randrange_func.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""For getting random integer values
+"""
+
+from random import randrange, randint
+
+print(randrange(1), end=' ')
+print(randrange(0, 1), end=' ')
+print(randrange(0, 1, 1), end=' ')
+print(randint(0, 1))
