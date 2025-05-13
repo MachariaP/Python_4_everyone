5862f511eb6f9bbec96d6e71199a65d88dc66181
Phinehas <walburphinehas78@gmail.com>
2025-05-13 10:08:13 +0300

    Add main_new.py with sys.path.append to import module from parent directory
    - Implemented script to import module.py from ../modules using sys.path.append('..\modules').
    - Uses module.suml and module.prodl to compute sum and product of zero and one lists.
    - Demonstrates dynamic module path configuration and explicit namespace usage.

--- modules/my_first_module/main.py
+++ modules/my_first_module/main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
+from sys import path
+
+path.append('..\\modules')
+
 import module
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))
