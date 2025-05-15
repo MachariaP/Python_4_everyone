ef5a2090dcd16f1718fa750dc788038210f60247
Phinehas <walburphinehas78@gmail.com>
2025-05-15 12:18:21 +0300

    Implemented an object-oriented Stack class with push and pop methods, demonstrating encapsulation and stack operations with example usage.

---
+++ stack/1-main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# The stack - the object approach.
+
+# Defining the Stack class.
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+stack_object = Stack()
+
+stack_object.push(1)
+stack_object.push(2)
+stack_object.push(3)
+stack_object.push(4)
+stack_object.push(5)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
